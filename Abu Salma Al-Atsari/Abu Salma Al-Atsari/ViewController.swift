27cba5b3da0891f2036dc2f6a355a20476ec59fe
Abi SA <abisa3002@gmail.com>
2017-12-02 16:09:43 +0700

    Done

---
+++ Abu Salma Al-Atsari/Abu Salma Al-Atsari/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Abu Salma Al-Atsari
+//
+//  Created by Abi Sayuti on 12/2/17.
+//  Copyright © 2017 Abi Sayuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var AbuSalma: UIWebView!
+    
+      let urlweb = "https://abusalma.net/"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let requesturl = NSURL(string: urlweb)
+        
+        
+        let request = NSURLRequest(url: requesturl! as URL)
+        AbuSalma.loadRequest(request as URLRequest)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
